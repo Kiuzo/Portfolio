fd52c3999b221ba9cb45c0dd5acf486cad594b30
kiuzo <david.soares2411@gmail.com>
2025-12-12 16:57:59 -0300

    fix: deploy v3

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
   devIndicators: false,
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
 };
 
 export default nextConfig;
